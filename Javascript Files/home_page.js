5fc66cbebe8951490f7da445ac42b2d489a56545
Fakhri <fakhrimg@code.edu.az>
2023-06-02 09:09:20 +0400

    home page functions were updated

--- Javascript Files/home_page.js
+++ Javascript Files/home_page.js
@@ -23,14 +23,15 @@ const dropdown = document.getElementById("dropdown");
 const closeBtn = document.getElementById("closeBtn");
 
 burger.addEventListener("click", () => {
-  dropdown.style.display = "flex";
   burger.style.display = "none";
+  dropdown.style.right = "0px";
 });
 
 closeBtn.addEventListener("click", () => {
-  dropdown.style.display = "none";
+  dropdown.style.right = "-400px";
   burger.style.display = "initial";
 });
+
 
 const info_clock = document.getElementById("info_right");
 const buttons_div = document.createElement("div");
